5193b43964e92a0a4ec45bee65682f759a16395c
Italo Castillo <italogitdoe@gmail.com>
2024-06-19 23:40:36 +0200

    fix header size

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,26 +7,26 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header
-      className={cn(
-        "flex flex-row flex-no-wrap content-center justify-between items-center p-3  m-auto gap-5 w-full sticky top-0 bg-zinc-200"
-      )}
+      className={cn(" items-center p-3 w-full   sticky top-0 bg-zinc-200")}
     >
-      <Link href={"/"}>
-        <Avatar>
-          <AvatarImage
-            src="https://visualenglishschool.com/wp-content/uploads/2019/10/English-Movie-Club-Milano.jpg"
-            alt="icon"
-          />
-          <AvatarFallback>MOVIE</AvatarFallback>
-        </Avatar>
-      </Link>
-      <div className="w-96">
-        <Input type="text" placeholder="search ...." />
-      </div>
-      <div>
-        <Button variant="outline" size={"icon"}>
-          ES
-        </Button>
+      <div className="flex flex-row flex-no-wrap items-center  md:container m-auto justify-between gap-3">
+        <Link href={"/"}>
+          <Avatar>
+            <AvatarImage
+              src="https://visualenglishschool.com/wp-content/uploads/2019/10/English-Movie-Club-Milano.jpg"
+              alt="icon"
+            />
+            <AvatarFallback>MOVIE</AvatarFallback>
+          </Avatar>
+        </Link>
+        <div className=" w-40 lg:w-96 sm:gap-x-56 ">
+          <Input type="text" placeholder="search ...." />
+        </div>
+        <div>
+          <Button variant="outline" size={"icon"}>
+            ES
+          </Button>
+        </div>
       </div>
     </header>
   );
